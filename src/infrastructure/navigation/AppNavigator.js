f0eabeeb843ab44ce27552f5d4294159b6e8e2fe
Shay Avraham <61618334+ShayAvraham@users.noreply.github.com>
2021-07-28 00:10:03 +0300

    added RSP home page navigator

--- src/infrastructure/navigation/AppNavigator.js
+++ src/infrastructure/navigation/AppNavigator.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { logout } from '../../features/account/slices/userSlice';
+import { logout } from '../../features/account/slices/userSlice'; // should be move to 'settings' screen
 import { Text, View, Button } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons';
+import { colors } from '../theme/colors';
+
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Calendar: "calendar-outline",
+  History: "construct-sharp",
+  Settings: "settings-outline",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => ( 
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+// temporary until all screen will be created
+const Calendar = () => <Text>Calendar</Text>
+const History = () => <Text>History</Text>
+const Settings = () => <Text>Settings</Text>
+
 
 export default function AppNavigator() {
   const dispatch = useDispatch();
   return (
-    <View
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello, world!</Text>
-      <Button
-        onPress={() => dispatch(logout())}
-        title='logout'
-        color='#841584'
-      />
-    </View>
+        <Tab.Navigator
+            initialRouteName="Calendar"
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: colors.brand.primary,
+              inactiveTintColor: colors.brand.muted,
+            }}
+          >
+            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="History" component={History} />
+            <Tab.Screen name="Calendar" component={Calendar} />
+          </Tab.Navigator>
   );
 }
